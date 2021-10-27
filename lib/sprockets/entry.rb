ac9f9891fdc24f427fa46c24bf3f6ad590507f29
Lachlan Sylvester <lachlan.sylvester@3plearning.com>
2021-10-27 13:12:26 +1100

    look up posibily matching entries by walking the in memory entry tree

--- lib/sprockets/entry.rb
+++ lib/sprockets/entry.rb
@@ -8,8 +8,15 @@ module Sprockets
     def find_matching_path_for_extensions(logical_name, extensions)
       dirname  = File.dirname(File.join(@path, logical_name))
       basename = File.basename(logical_name)
+
+      *directory_names, filename = logical_name.split('/')
+      directory_for_logical_path = directory_names.inject(self){ |directory, path| directory.entries[path] }
+
       matches = []
-      entries(dirname).each do |entry|
+
+      return matches unless directory_for_logical_path
+
+      directory_for_logical_path.entries.keys.each do |entry|
         next unless File.basename(entry).start_with?(basename)
         extname, value = match_path_extname(entry, extensions)
         if basename == entry.chomp(extname)
@@ -39,8 +46,12 @@ module Sprockets
       end
     end
 
-    def entries(path)
-      @env.entries(path)
+    def entries
+      @entries ||= begin
+        @env.entries(@path).each_with_object({}) do |path, result|
+          result[path] = Entry.new(@env, File.join(@path, path))
+        end
+      end
     end
 
     def match_path_extname(path, extensions)
